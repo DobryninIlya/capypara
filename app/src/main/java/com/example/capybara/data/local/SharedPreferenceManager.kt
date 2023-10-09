ca43e57f79507c29bfe124bc6b4a82ed733476ac
KevinBacon <turnipsh@student.21-school.ru>
2023-10-10 00:18:55 +0300

    start to implement SharedPreferenceManager

--- app/src/main/java/com/example/capybara/data/local/SharedPreferenceManager.kt
+++ app/src/main/java/com/example/capybara/data/local/SharedPreferenceManager.kt
@@ -1,10 +1,23 @@
 package com.example.capybara.data.local
 
-class SharedPreferenceManager {
-    fun saveUid(uid: String) {}
-    fun getUid(): String {
-        TODO()
-    }
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.capybara.R
 
+class SharedPreferenceManager(context: Context) {
+
+    private val UID_KEY = "uid"
+
+    private val sharedPreference: SharedPreferences = context.getSharedPreferences(
+        context.getString(R.string.preference_file_key),
+        Context.MODE_PRIVATE
+    )
+
+    fun saveUid(uid: String) =
+        sharedPreference.edit().putString(UID_KEY, uid).apply()
+
+    fun getUid(): String? {
+        return sharedPreference.getString(UID_KEY, "")
+    }
 
 }
